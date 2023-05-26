630c65ccd0d51829486a4a7c953b716b35a5e4e3
vaibhav519 <vaibhav3raj@gmail.com>
2023-05-26 17:48:05 +0530

    built UserWidget

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Navbar from "scenes/navbar";
 import { Box } from "@mui/material";
+import {useMediaQuery} from "@mui/material";
+import { useSelector } from "react-redux";
+import UserWidget from "scenes/widgets/UserWidgets";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
